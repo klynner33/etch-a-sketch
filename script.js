4494eeb610ea1aae630e8fe25ffb4c5bb1b7e900
Kate Lynner <113389068+klynner33@users.noreply.github.com>
2023-02-27 21:40:24 -0600

    Added a for loop to create 16 rows of divs

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+let numOfRows = 16;
+    for (let i = 0; i < numOfRows; i++) {
+        const rowDiv = document.createElement("div");
+         
+        container.appendChild(rowDiv);
+    }
+
+
+
+
+ 
