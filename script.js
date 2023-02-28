538b83a45a8dc915a5f6871c858ce08ef004b50f
Kate Lynner <113389068+klynner33@users.noreply.github.com>
2023-02-27 22:17:57 -0600

    Used the for loop to make 256 divs and added the class squares to all divs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
 
-let numOfRows = 16;
-    for (let i = 0; i < numOfRows; i++) {
-        const rowDiv = document.createElement("div");
-         
-        container.appendChild(rowDiv);
+ 
+
+let numOfSquares = 256;
+for (let i = 0; i < numOfSquares; i++) {
+    const squares = document.createElement("div");
+    squares.classList.add("squares");
+    container.appendChild(squares);
     }
 
 
