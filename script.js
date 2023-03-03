7ec2ccad9545913b3714d87955335bc31e2ec1dd
Kate Lynner <113389068+klynner33@users.noreply.github.com>
2023-03-02 23:24:29 -0600

    Working to get grid to change sizes

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ const squares = document.querySelector(".squares");
 const resetBtn = document.querySelector("#reset-btn");
 const blackBtn = document.querySelector("#black-btn");
 const colorBtn = document.querySelector("#color-btn");
+const eraseBtn = document.querySelector("#erase-btn");
 
 
 
-
-function gridSize(num) {
-    let totalSquares = num * num;
-    return totalSquares;
-}
-
-console.log(gridSize(16));
-let numOfSquares = gridSize();
+let size = 16;
+let numOfSquares = size * size;
 for (let i = 0; i < numOfSquares; i++) {
     const squares = document.createElement("div");
     squares.classList.add("squares");
@@ -30,11 +25,22 @@ for (let i = 0; i < numOfSquares; i++) {
             squares.style.backgroundColor = generateRandomColor();
         });
     });
+
+    eraseBtn.addEventListener("click", () => {
+        squares.addEventListener("mouseover", () => {    
+            squares.style.backgroundColor = "white";
+        });
+    });
 }
 
 function generateRandomColor() {
     let hex = "#" + Math.floor(Math.random() * 16777215).toString(16);
     return hex;
+}
+
+function setupGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr`
 }
 
 resetBtn.addEventListener("click", function(e) {
